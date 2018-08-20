538b4fc3cf7e003abc5ae90360398403261f32af
Elias Mikkola <elias.mikkola1@gmail.com>
2018-08-20 18:34:58 +0300

    3.7 puhelinluettelon backend osa 7

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+app.use(morgan('tiny')
+)
 
 app.use(bodyParser.json())
+
 
 
 let persons = [
