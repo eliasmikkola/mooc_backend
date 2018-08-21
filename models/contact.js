2e4e39240ff8d9aba35a5aea77c97a9b041f8dd1
Elias Mikkola <elias.mikkola1@gmail.com>
2018-08-21 03:27:06 +0300

    add production check on dotenv  3.21* eriytetty sovelluskehitys- ja tuotantotietokanta

--- models/contact.js
+++ models/contact.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
-require('dotenv').config();
+
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
 const url = process.env.MONGO_URL
 
 
